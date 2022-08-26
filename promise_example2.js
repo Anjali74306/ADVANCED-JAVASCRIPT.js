bc00f7e60b32a6e15772cfe6480b81c7994cbfd8
Anjali74306 <89630072+Anjali74306@users.noreply.github.com>
2022-08-26 19:28:48 +0530

    Add files via upload

---
+++ promise_example2.js
@@ -0,0 +1,42 @@
+//  var promise=new Promise( function(reject,resolve){
+ const pobj1 =new Promise((resolve,reject)=>{ 
+     setTimeout( ()=>{
+         let roll_no=[1,2,3,4,5];
+         resolve(roll_no);
+     },2000);
+ } );
+
+ const getbiodata=(indexdata)=>{
+     return new Promise((resolve, reject)=>{
+         setTimeout((indexdata)=>{
+             let biodata={
+                 name:"anjali",
+                 age: 26
+             }
+             resolve(`my roll no. is ${indexdata}. My name is ${biodata.name} and I am ${biodata.age}years old.`);
+
+         },2000,indexdata)
+     });
+ }
+ 
+ pobj1.then((roll_no) =>{
+     console.log(roll_no);
+     return getbiodata(roll_no[1]);
+
+ }).then((kuchbhi)=>{
+     console.log(kuchbhi);
+ }).catch((error)=>{
+     console.log(error);
+ })
+
+
+
+
+
+
+
+
+
+
+
+
